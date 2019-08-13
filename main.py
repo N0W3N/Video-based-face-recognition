b5d34f6224aefb24926a0440a259f06b4b9e1ecb
N0W3N <15861569+N0W3N@users.noreply.github.com>
2019-08-13 23:21:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import cv2
+import sys
 
-cascPath = "haarcascade_frontalface_default.xml"
+# needed input-files via cli as arguments
+filePath = sys.argv[0]
+cascPath = sys.argv[1]
 
+# create a stream and cascade file for the process
+
+stream = cv2.VideoCapture(filePath)
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-stream = cv2.VideoCapture('test.mp4')
+# read stream input and convert each frame into a processable file with the given color space
 
 while stream.isOpened():
     ret, frame = stream.read()
@@ -12,17 +18,26 @@ while stream.isOpened():
     faces = faceCascade.detectMultiScale\
             (
             gray,
-            scaleFactor = 1.2,
+            scaleFactor = 1.2, 
             minNeighbors = 5,
             minSize = (30, 20),
             flags = cv2.CASCADE_SCALE_IMAGE
-        )
+            )
+    # draw a rectangle around the faces
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+    # show the current frame with a reactangle around the existing faces
+        
     cv2.imshow('Frame', frame)
+    
+    # simple break / close statement 
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+# release() and destroyAllWindows() are needed, otherwise it can result in performance issues on low-spec systems
 
 stream.release()
 cv2.destroyAllWindows()
